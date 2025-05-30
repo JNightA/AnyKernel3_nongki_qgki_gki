80aba99c4b566e70c1ad8fa2ec94208ee9c766f4
JNightA <och95989076@163.com>
2025-05-30 09:21:11 +0800

    添加gki分支

--- anykernel.sh
+++ anykernel.sh
@@ -4,13 +4,17 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Chisato Kernel by Kinaxie @ coolapk 猫羽今天吃什么
-do.devicecheck=1
+kernel.string= ShirkNeko patched kernel with SukiSU+KPM
+do.devicecheck=0
 do.modules=0
-do.systemless=1
+do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=<DEVICE>
+device.name1=
+device.name2=
+device.name3=
+device.name4=
+device.name5=
 supported.versions=
 supported.patchlevels=
 supported.vendorpatchlevels=
@@ -18,65 +22,33 @@ supported.vendorpatchlevels=
 
 
 ### AnyKernel install
-## boot files attributes
-boot_attributes() {
-set_perm_recursive 0 0 755 644 $RAMDISK/*;
-set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
-} # end attributes
-
-# boot shell variables
-# Check if the device uses A/B partitions
-if [ -e /dev/block/bootdevice/by-name/boot_a ]; then
-  # Get the current active slot
-  SLOT=$(getprop ro.boot.slot_suffix)
-  if [ -z "$SLOT" ]; then
-    SLOT=_$(getprop ro.boot.slot)
-  fi
-  # Set the BLOCK variable to the correct boot partition
-  BLOCK=/dev/block/bootdevice/by-name/boot$SLOT
-else
-  # For non-A/B devices, use the standard boot partition
-  BLOCK=/dev/block/bootdevice/by-name/boot
-fi
-
-IS_SLOT_DEVICE=0;
-RAMDISK_COMPRESSION=auto;
-PATCH_VBMETA_FLAG=auto;
+## boot shell variables
+block=boot
+is_slot_device=auto
+ramdisk_compression=auto
+patch_vbmeta_flag=auto
+no_magisk_check=1
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
-. tools/ak3-core.sh;
+. tools/ak3-core.sh
+
+kernel_version=$(cat /proc/version | awk -F '-' '{print $1}' | awk '{print $3}')
+case $kernel_version in
+    5.1*) ksu_supported=true ;;
+    6.1*) ksu_supported=true ;;
+    6.6*) ksu_supported=true ;;
+    *) ksu_supported=false ;;
+esac
+
+ui_print " " "  -> ksu_supported: $ksu_supported"
+$ksu_supported || abort "  -> Non-GKI device, abort."
 
 # boot install
-echo "Unpacking ramdisk..."
-dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
-echo "Ramdisk unpacked!"
-
-# init.rc
-echo "Modifying init.rc..."
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-echo "init.rc modified!"
-
-# init.tuna.rc
-echo "Modifying init.tuna.rc..."
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
-echo "init.tuna.rc modified!"
-
-# fstab.tuna
-echo "Modifying fstab.tuna..."
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
-echo "fstab.tuna modified!"
-
-echo "Writing to boot partition..."
-write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
-echo "Boot partition written!"
-
-echo "Flashing complete!"
-exit 0
+if [ -L "/dev/block/bootdevice/by-name/init_boot_a" -o -L "/dev/block/by-name/init_boot_a" ]; then
+    split_boot # for devices with init_boot ramdisk
+    flash_boot # for devices with init_boot ramdisk
+else
+    dump_boot # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+    write_boot # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+fi
 ## end boot install
