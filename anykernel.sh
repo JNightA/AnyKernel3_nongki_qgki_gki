f6d043e2da9884ed36c2183031151f1c3641ab42
JNightA <och95989076@163.com>
2025-05-30 11:56:39 +0800

    使用章鱼坤配置

--- anykernel.sh
+++ anykernel.sh
@@ -4,13 +4,13 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Chisato Kernel by Kinaxie @ coolapk 猫羽今天吃什么
-do.devicecheck=1
+kernel.string=ExampleKernel by osm0sis @ xda-developers
+do.devicecheck=0
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=<DEVICE>
+device.name1=star
 supported.versions=
 supported.patchlevels=
 supported.vendorpatchlevels=
@@ -25,21 +25,8 @@ set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
 } # end attributes
 
 # boot shell variables
-# Check if the device uses A/B partitions
-if [ -e /dev/block/bootdevice/by-name/boot_a ]; then
-  # Get the current active slot
-  SLOT=$(getprop ro.boot.slot_suffix)
-  if [ -z "$SLOT" ]; then
-    SLOT=_$(getprop ro.boot.slot)
-  fi
-  # Set the BLOCK variable to the correct boot partition
-  BLOCK=/dev/block/bootdevice/by-name/boot$SLOT
-else
-  # For non-A/B devices, use the standard boot partition
-  BLOCK=/dev/block/bootdevice/by-name/boot
-fi
-
-IS_SLOT_DEVICE=0;
+BLOCK=boot;
+IS_SLOT_DEVICE=1;
 RAMDISK_COMPRESSION=auto;
 PATCH_VBMETA_FLAG=auto;
 
@@ -47,36 +34,7 @@ PATCH_VBMETA_FLAG=auto;
 . tools/ak3-core.sh;
 
 # boot install
-echo "Unpacking ramdisk..."
 dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
-echo "Ramdisk unpacked!"
 
-# init.rc
-echo "Modifying init.rc..."
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-echo "init.rc modified!"
-
-# init.tuna.rc
-echo "Modifying init.tuna.rc..."
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
-echo "init.tuna.rc modified!"
-
-# fstab.tuna
-echo "Modifying fstab.tuna..."
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
-echo "fstab.tuna modified!"
-
-echo "Writing to boot partition..."
 write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
-echo "Boot partition written!"
-
-echo "Flashing complete!"
-exit 0
 ## end boot install
