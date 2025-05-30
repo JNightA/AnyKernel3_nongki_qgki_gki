c77c0137948a462d00af4ef455b8378b538e2005
JNightA <och95989076@163.com>
2025-05-30 12:07:52 +0800

    同步章鱼坤配置

--- anykernel.sh
+++ anykernel.sh
@@ -1,40 +1,71 @@
-### AnyKernel3 Ramdisk Mod Script
-## osm0sis @ xda-developers
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
 
-### AnyKernel setup
-# global properties
+## AnyKernel setup
+# begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
 do.devicecheck=0
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=star
+device.name1=alioth
+device.name2=aliothin
+device.name3=apollo
+device.name4=apolloin
+device.name5=lmi
 supported.versions=
 supported.patchlevels=
-supported.vendorpatchlevels=
 '; } # end properties
 
+# shell variables
+block=boot;
+is_slot_device=auto;
+ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
+no_block_display=1
 
-### AnyKernel install
-## boot files attributes
-boot_attributes() {
-set_perm_recursive 0 0 755 644 $RAMDISK/*;
-set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
-} # end attributes
-
-# boot shell variables
-BLOCK=boot;
-IS_SLOT_DEVICE=1;
-RAMDISK_COMPRESSION=auto;
-PATCH_VBMETA_FLAG=auto;
-
-# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-# boot install
-dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+## Select the correct image to flash
+userflavor="$(file_getprop /system/build.prop "ro.build.flavor")";
+case "$userflavor" in
+    aospa_alioth-user) os="aospa"; os_string="Paranoid Android ROM";;
+    aospa_apollo-user) os="aospa"; os_string="Paranoid Android ROM";;
+    aospa_lmi-user) os="aospa"; os_string="Paranoid Android ROM";;
+    missi-user) os="miui"; os_string="MIUI ROM";;
+    missi_phoneext4_cn-user) os="miui"; os_string="MIUI ROM";;
+    missi_phone_cn-user) os="miui"; os_string="MIUI ROM";;
+    qssi-user) os="miui"; os_string="MIUI ROM";;
+    *) os="aosp"; os_string="AOSP ROM";;
+esac;
+ui_print "  -> $os_string is detected!";
 
-write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+mv $home/kernels/Image $home/Image;
+
+# If you have some problems with the new kernel, try to uncomment the following line to also flash the [dtb].
+# mv $home/kernels/dtb $home/dtb;
+
+## AnyKernel boot install
+split_boot;
+
+flash_boot;
+# flash_dtbo;
 ## end boot install
+
+# Vendor boot
+#block=vendor_boot;
+#is_slot_device=1;
+#ramdisk_compression=auto;
+#patch_vbmeta_flag=auto;
+
+# reset for vendor_boot patching
+#reset_ak;
+
+## AnyKernel vendor_boot install
+#split_boot;
+
+#flash_boot;
+## end vendor_boot install
